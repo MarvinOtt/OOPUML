d6b4420e5b8e7b558dcd16c438f7925bf4f6cc7d
Alexander <a.schoenhofer@htlstp.at>
2018-12-17 14:24:44 +0100

    person vervollstaendigt

---
+++ src/Schueler.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Schueler extends Person
+{
+    private int katalognummer;
+    private boolean eigenberechtigt;
+    private Date eintrittsdatum;
+
+    public int getKatalognummer()
+    {
+        return katalognummer;
+    }
+
+    public boolean isEigenberechtigt()
+    {
+        return eigenberechtigt;
+    }
+    public Date getEintrittsdatum()
+    {
+        return eintrittsdatum;
+    }
+
+}
