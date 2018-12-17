51af06302a1f03db17127d5ff41ed0117ec3b5ea
Marvin Ott <marvinott20@gmail.com>
2018-12-17 14:37:37 +0100

    test

--- src/Schule.java
+++ src/Schule.java
@@ -1,4 +1,13 @@
 public class Schule
 {
-
+	private String name;
+	private Long schulkennzahl;
+	private String schultyp;
+	
+	public boolean addPersonal(NichtLehrpersonal NLP)
+	{
+		return false;
+	}
+	
+	
 }
