13baa0f55430520a534650df5ba752511e84bade
Marvin Ott <marvinott20@gmail.com>
2018-12-17 14:10:20 +0100

    test

--- src/Addresse.java
+++ src/Addresse.java
@@ -1,4 +1,20 @@
 public class Addresse
 {
-
+	private String ort;
+	private String strasse;
+	private Integer hausnummer;
+	private Integer plz;
+	
+	public String getOrt(){return ort;}
+	
+	public String getStrasse(){return strasse;}
+	
+	public Integer getHausnummer(){return hausnummer;}
+	
+	public Integer getPlz(){return plz;}
+	
+	public void printAddresse()
+	{
+		System.out.println(plz + " " + ort + " " + strasse + " " + hausnummer);
+	}
 }
