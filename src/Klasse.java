c2b4a1f3edcf1629ce3666b6bb324638c846abbb
Alexander <a.schoenhofer@htlstp.at>
2018-12-17 14:50:13 +0100

    temporär

---
+++ src/Klasse.java
@@ -0,0 +1,34 @@
+public class Klasse
+{
+    private String bezeichnung;
+    private int schulstufe;
+
+    public String getBezeichnung()
+    {
+        return bezeichnung;
+    }
+
+    public int getSchulstufe()
+    {
+        return schulstufe;
+    }
+
+    public float getDurchschnittsalter()
+    {
+
+    }
+
+    public boolean setKlassensprecher(Schueler schueler)
+    {
+        return true;
+    }
+
+    public boolean addSchueler(Schueler schueler)
+    {
+        return true;
+    }
+    public void getStundenplan()
+    {
+
+    }
+}
