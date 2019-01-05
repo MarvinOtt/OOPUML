d5c6e2717f75cd4de97c2b1e233f63eebaa76a3c
Alexander <a.schoenhofer@htlstp.at>
2019-01-05 14:32:12 +0100

    Raum unvollstaendig

--- src/Fach.java
+++ src/Fach.java
@@ -2,5 +2,20 @@ public class Fach
 {
     private String name;
     private int wochenstunden;
+    private Raumtyp raumanforderung;
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getWochenstunden()
+    {
+        return wochenstunden;
+    }
+
+    public Raumtyp getRaumanforderung()
+    {
+        return raumanforderung;
+    }
 }
