42a0af22e31656dab7ce3e4c890747aa2f6cbf48
Alexander <a.schoenhofer@htlstp.at>
2019-01-07 14:43:26 +0100

    Klasse erweitert

--- src/Lehrer.java
+++ src/Lehrer.java
@@ -3,13 +3,11 @@ import java.util.HashSet;
 public class Lehrer extends Mitarbeiter
 {
     private String kuerzel, name;
-    private HashSet<Fach> zuunterrichtendefächer;
     
     public Lehrer(String kuerzel, String name)
     {
         super(name);
         this.kuerzel = kuerzel;
-        zuunterrichtendefächer = new HashSet<>();
     }
     
     public String getKuerzel()
@@ -17,10 +15,7 @@ public class Lehrer extends Mitarbeiter
         return kuerzel;
     }
     
-    public void addFach(Fach fach)
-    {
-        zuunterrichtendefächer.add(fach);
-    }
+
     public void exportStundenplan()
     {
     
