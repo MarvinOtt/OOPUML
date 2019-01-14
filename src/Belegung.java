6a871647c1f7c3f6262339f5506da011267fcb89
Alexander <a.schoenhofer@htlstp.at>
2019-01-14 13:59:19 +0100

    Klasse erweitert

--- src/Belegung.java
+++ src/Belegung.java
@@ -39,6 +39,6 @@ public class Belegung
     
     public String getBelegungsstring()
     {
-        return "Tag: " + tag.name() + " | " + "Stunde " + unterrichtseinheit + " | " + "Raum:" + raum.getRaumNummer() + " | " + "Klasse: " + klasse.getBezeichnung() +  " | " + "Fach: " + unterrichtsfach.getName() + " | "     + "Lehrer: " + lehrer.getKuerzel();
+        return "Tag: " + tag.name() + " | " + "Stunde " + unterrichtseinheit + " | " + "Raum: " + raum.getRaumNummer() + " | " + "Klasse: " + klasse.getBezeichnung() +  " | " + "Fach: " + unterrichtsfach.getName() + " | "     + "Lehrer: " + lehrer.getKuerzel();
     }
 }
